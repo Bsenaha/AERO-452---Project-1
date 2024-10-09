7c1bcfa05ca4d3875e5d0165bec95fc41cd56534
Brandon <brandonsenaha@gmail.com>
2024-10-09 10:55:53 -0700

    testing push

---
+++ pull.m
@@ -0,0 +1,4 @@
+repo = gitclone("https://github.com/Bsenaha/AERO-452---Project-1.git");
+
+%repo = gitrepo("C:\Users\Brando\OneDrive\Documents\MATLAB\4th year\Fall\452 - Spaceflight Dynamics\Project 1");
+
