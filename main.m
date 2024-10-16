f35eebeb0f453f86a1f17508c467505f397f2ca8
Brandon <brandonsenaha@gmail.com>
2024-10-15 20:39:31 -0700

    FIRST project commit - GEO satellite defined

--- main.m
+++ main.m
@@ -1,1 +1,48 @@
-% hello testing 
+%% AERO 452 - SPACEFLIGHT DYNAMICS PROJECT 1
+
+% CALEB ARBRETON, BRANDON SENAHA
+% MAIN PROJECT SCRIPT
+
+clc; clear; close all
+
+%% PROJECT INFORMATION
+
+% Total Time to Capture (TTC): 10 days
+% Initial Relative Distance: 100 km
+
+% Holds:
+%   1. 20-40 km (w/ small rel speed)
+%   2. 1     km
+%   3. 0.300 km
+%   4. 20    m  (w/ small rel speed)
+
+% Satellite Information
+% Name: OPS 9381 (GGTS)
+% **will be referenced as "OPS"**
+% Orbit Type: GEO
+
+% Heavens above orbit link:
+% https://www.heavens-above.com/orbit.aspx?satid=2207&lat=0&lng=0&loc=Unspecified&alt=0&tz=UCT&cul=en
+
+%% TLE PROCESSING
+
+% read TLE to struct
+OPS_TLE = tleread('TLE'); % all angle measurements in degrees
+
+
+%% ID CHASER ORBIT
+
+
+%% DEFINING STATES
+
+
+%% MANEUVER 1
+
+
+%% HOLD 1
+
+
+%% ...
+
+
+%% RESULTS
